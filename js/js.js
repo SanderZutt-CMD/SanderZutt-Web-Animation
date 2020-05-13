a89cffeb78f1a89df3f1b8f28f66811f0ae0f099
sanderzutt <43617759+sanderzutt@users.noreply.github.com>
2020-05-13 22:22:26 +0200

    JS poging

---
+++ js/js.js
@@ -0,0 +1,24 @@
+// var name = 'Sander';
+// var sureName = ' Zutt';
+//
+// console.log(name + sureName)
+
+
+// class zetten op de letter Y
+var LetterY = document.querySelectorAll('path')[4];
+var YTwee = document.querySelectorAll('path')[5];
+
+LetterY.addEventListener('click', function () {
+	YTwee.classList.toggle('Ytoetsenbord');
+});
+
+
+
+
+
+// var OpenMenuBalk = document.querySelectorAll('li')[5];
+// var Menu = document.querySelector('ul');
+//
+// OpenMenuBalk.addEventListener('click', function () {
+// 	Menu.classList.toggle('keuzedown');
+// });
